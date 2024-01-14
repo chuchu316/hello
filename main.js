3d2db77ee78a8e08036508e1be6ee28738f590d3
chuchu316 <153891397+chuchu316@users.noreply.github.com>
2024-01-15 03:30:17 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+function login() {
+    // 實際登入的程式碼可以在這裡實現
+    alert("登入成功！");
+    closeModal('loginModal');
+}
+
+function register() {
+    // 實際註冊的程式碼可以在這裡實現
+    alert("註冊成功！");
+    closeModal('registerModal');
+}
